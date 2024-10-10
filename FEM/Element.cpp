13353e8acb80e3085727b71595bacc3a031a659a
Alekseev Andrey <alekseev.anrdey@nanocad.ru>
2024-10-10 23:00:10 +0300

    Drop files from .gitignore

--- FEM/Element.cpp
+++ FEM/Element.cpp
@@ -1,55 +1,97 @@
-#include "Element.h"
-#include "Equation.h"
-
-Element::Element(const Node& node1, const Node& node2)
-{
-	Nodes[0] = node1;
-	Nodes[1] = node2;
-
-	for (int i = 0; i < 2; i++)
-	{
-		LoadVector[i] = 0.0;
-		for (int j = 0; j < 2; j++) {
-			StiffnessMatrix[i][j] = 0.0;
-		}
-	}
-}
-
-//void Element::CalculateStiffnessMatrix() {
-//	double h = Nodes[1].GetX() - Nodes[0].GetX(); // Длина элемента
-//	StiffnessMatrix[0][0] = 1.0 / h + 1; // u
-//	StiffnessMatrix[0][1] = -1.0 / h - 1; // v
-//	StiffnessMatrix[1][0] = -1.0 / h + 1; // -2v
-//	StiffnessMatrix[1][1] = 1.0 / h - 1;  // v
+//#include "Element.h"
+//#include "Equation.h"
+//
+//template<int Degree>
+//Element<Degree>::Element(const Node& node1, const Node& node2)
+//{
+//	Nodes[0] = node1;
+//	Nodes[Degree] = node2;
+//
+//	for (int i = 1; i < Degree; ++i)
+//	{
+//		Nodes[i] = Node::Interpolate(node1, node2, i / double(Degree));
+//	}
+//
+//	for (int i = 0; i < Degree + 1; ++i) {
+//		LoadVector[i] = 0.0;
+//		for (int j = 0; j < Degree + 1; ++j) {
+//			StiffnessMatrix[i][j] = 0.0;
+//		}
+//	}
 //}
-
-void Element::CalculateStiffnessMatrix(SecondOrderDifferentialEq& eq) {
-	double a = eq.getA();
-	double b = eq.getB();
-	double c = eq.getC();
-	double d = eq.getD();
-
-	double h = Nodes[1].GetX() - Nodes[0].GetX(); // Длина элемента
-	StiffnessMatrix[0][0] = -(a / h) + (c * h/3) - (b / 2);
-	StiffnessMatrix[0][1] = (a / h) + (c * h/6) + (b / 2);
-	StiffnessMatrix[1][0] = (a / h) - (c * h/6)  - (b / 2);
-	StiffnessMatrix[1][1] = -(a / h) + (c * h/3) + (b / 2);
-}
-
-
-//void Element::CalculateLoadVector() {
-//	double h = Nodes[1].GetX() - Nodes[0].GetX(); // Длина элемента
-//	LoadVector[0] = -5.0 * h / 2.0; // Нагрузка для u
-//	LoadVector[1] = -5.0 * h / 2.0; // Нагрузка для v
+//
+//
+//template<int Degree>
+//Element<Degree>::Element(const std::array<Node, Degree + 1>& elementNodes)
+//{
+//	Nodes = elementNodes;
+//	for (int i = 0; i < Degree + 1; ++i) {
+//		LoadVector[i] = 0.0;
+//		for (int j = 0; j < Degree + 1; ++j) {
+//			StiffnessMatrix[i][j] = 0.0;
+//		}
+//	}
 //}
-
-void Element::CalculateLoadVector(SecondOrderDifferentialEq& eq) {
-	double a = eq.getA();
-	double b = eq.getB();
-	double c = eq.getC();
-	double d = eq.getD();
-
-	double h = Nodes[1].GetX() - Nodes[0].GetX(); // Длина элемента
-	LoadVector[0] =   - d * h / 2.0; // Нагрузка для u
-	LoadVector[1] =  - d * h / 2.0; // Нагрузка для v
-}
+//
+//
+//template<int Degree>
+//void Element<Degree>::CalculateStiffnessMatrix(SecondOrderDifferentialEq& eq) {
+//	double a = eq.getA();
+//	double b = eq.getB();
+//	double c = eq.getC();
+//	double d = eq.getD();
+//	double h = Nodes[1].GetX() - Nodes[0].GetX(); // Длина элемента
+//	if constexpr (Degree == 1)
+//	{
+//
+//		StiffnessMatrix[0][0] = -(a / h) + (c * h / 3) - (b / 2);
+//		StiffnessMatrix[0][1] = (a / h) + (c * h / 6) + (b / 2);
+//		StiffnessMatrix[1][0] = (a / h) - (c * h / 6) - (b / 2);
+//		StiffnessMatrix[1][1] = -(a / h) + (c * h / 3) + (b / 2);
+//	}
+//	else if constexpr (Degree == 3)
+//	{
+//		StiffnessMatrix[0][0];
+//		StiffnessMatrix[0][1];
+//		StiffnessMatrix[0][2];
+//		StiffnessMatrix[0][3];
+//			
+//		StiffnessMatrix[0][0];
+//		StiffnessMatrix[0][1];
+//		StiffnessMatrix[0][2];
+//			StiffnessMatrix[0][3];
+//
+//			StiffnessMatrix[0][0];
+//			StiffnessMatrix[0][1];
+//			StiffnessMatrix[0][2];
+//			StiffnessMatrix[0][3];
+//			
+//			StiffnessMatrix[0][0];
+//				StiffnessMatrix[0][1];
+//				StiffnessMatrix[0][2];
+//				StiffnessMatrix[0][3];
+//	}
+//}
+//
+//
+//template<int Degree>
+//void Element<Degree>::CalculateLoadVector(SecondOrderDifferentialEq& eq) {
+//	double a = eq.getA();
+//	double b = eq.getB();
+//	double c = eq.getC();
+//	double d = eq.getD();
+//
+//	double h = Nodes[1].GetX() - Nodes[0].GetX(); // Длина элемента
+//	if constexpr (Degree == 1)
+//	{
+//		LoadVector[0] = -d * h / 2.0; // Нагрузка для u
+//		LoadVector[1] = -d * h / 2.0; // Нагрузка для v
+//	}
+//	else if constexpr (Degree == 3)
+//	{
+//		LoadVector[0];
+//			LoadVector[0];
+//			LoadVector[0];
+//			LoadVector[0];
+//	}
+//}
